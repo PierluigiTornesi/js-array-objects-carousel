c780617e52f6b4730321bd01bdb9e03b6e9af764
PierluigiTornesi <pierluigi.tornesi02@gmail.com>
2023-11-07 23:57:55 +0100

    add: aggiunta commenti

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const slidesElem = document.querySelector(".slides");
+//parte destra
 const parteDestra = document.querySelector(".parteDestraImg")
 //creo la variabile che conterrá tutte le righe di codice che verranno scritte nell'html
 let imagesString = "";
+//righe della parte di destra
 let imageStringDestra = "";
 //ciclo per scrivere le righe di codice dentro la variabile 
 for (let i = 0; i < images.length; i++) {
@@ -18,12 +20,14 @@ const slidesElemsDestra = document.querySelectorAll(".image-destra")
 //visualizzo il primo elemento
 let currentSlideIndex = 0
 slidesElems[currentSlideIndex].classList.add("active")
+//aggiugo alle immagini di destra il bordino e tolgo la sfocatura
 slidesElemsDestra[currentSlideIndex].classList.add("attuale")
 //costante per l'ultima posizione dell'array 
 const fineArray = (images.length - 1)
 //titolo e testo delle immagini
 const titolo = document.getElementById("titolo")
 const testo = document.getElementById("testo")
+//inserisco nell'html il titolo e il testo della prima immagine nell'array di oggetti images che si trova nell'altro file js
 titolo.innerHTML = images[0].title;
 testo.innerHTML = images[0].text;
 
@@ -49,24 +53,29 @@ function prossima(){
         //vado all'immagine successiva
         //tolgo la classe active per far sparire l'immagine attuale
         slidesElems[currentSlideIndex].classList.remove("active");
+        //tolgo la classe attuale per togliere il bordino all'immagine attuale
         slidesElemsDestra[currentSlideIndex].classList.remove("attuale");
         //aumento l'indice
         currentSlideIndex++;
         //aggiungo la classe active per far apparire l'immagine successiva
         slidesElems[currentSlideIndex].classList.add("active");
+        //aggiungo la classe attuale per far apparire il bordino all'immagine successiva
         slidesElemsDestra[currentSlideIndex].classList.add("attuale");
     }else 
     //se ci troviamo all'ultima ricominciamo da capo
     if(currentSlideIndex === slidesElems.length -1){
       //tolgo la classe active per far sparire l'immagine attuale
       slidesElems[currentSlideIndex].classList.remove("active");
+      //tolgo la classe attuale per togliere il bordino all'immagine attuale
       slidesElemsDestra[currentSlideIndex].classList.remove("attuale");
       //imposto l'indice alla prima immagine
       currentSlideIndex = 0;
       //aggiungo la classe active per far apparire la prima immagine 
       slidesElems[currentSlideIndex].classList.add("active");
+      //aggiungo la classe attuale per far apparire il bordino alla prima immagine
       slidesElemsDestra[currentSlideIndex].classList.add("attuale");
     }
+    //cambio nell'html il titolo e il testo dell'immagine e li imposto con quelli dell'immagine successiva
     titolo.innerHTML= images[currentSlideIndex].title;
     testo.innerHTML= images[currentSlideIndex].text;
 }
@@ -77,24 +86,29 @@ function precedente(){
     if(currentSlideIndex > 0){
         //tolgo la classe active per far sparire l'immagine attuale
         slidesElems[currentSlideIndex].classList.remove("active");
+        //tolgo la classe attuale per togliere il bordino all'immagine attuale
         slidesElemsDestra[currentSlideIndex].classList.remove("attuale");
         //decremento l'indice
         currentSlideIndex--;
         //aggiungo la classe active per far apparire l'immagine precedente
         slidesElems[currentSlideIndex].classList.add("active");
+        //aggiungo la classe attuale per far apparire il bordino all'immagine successiva
         slidesElemsDestra[currentSlideIndex].classList.add("attuale");
     }else 
     //se ci troviamo alla prima passo all'ultima
     if(currentSlideIndex === 0){
     //tolgo la classe active per far sparire l'immagine attuale
       slidesElems[currentSlideIndex].classList.remove("active");
+      //tolgo la classe attuale per togliere il bordino all'immagine attuale
       slidesElemsDestra[currentSlideIndex].classList.remove("attuale");
       //imposto l'indice all'ultima immagine
       currentSlideIndex = fineArray;
       //aggiungo la classe active per far apparire l'ultima immagine 
       slidesElems[currentSlideIndex].classList.add("active");
+      //aggiungo la classe attuale per far apparire il bordino all'ultima immagine
       slidesElemsDestra[currentSlideIndex].classList.add("attuale");
     }
+    //cambio nell'html il titolo e il testo dell'immagine e li imposto con quelli dell'immagine precedente
     titolo.innerHTML= images[currentSlideIndex].title;
     testo.innerHTML= images[currentSlideIndex].text;
 }
